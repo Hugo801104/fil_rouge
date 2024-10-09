49ff213de21c3527ec5e6a5563e6cb508ff9b731
Hugo JASMIN <hugo.jasmin@imt-atlantique.net>
2024-10-09 15:06:56 +0200

    First implementation of PetriNet

--- src/fil_rouge/PetriNet.java
+++ src/fil_rouge/PetriNet.java
@@ -1,5 +1,43 @@
 package fil_rouge;
 
-public class PetriNet {
+import java.util.ArrayList;
 
+import fil_rouge.*;
+
+public class PetriNet {
+	
+	private ArrayList<Place> places;
+	private ArrayList<OutArc> outArcs;
+	private ArrayList<InArc> inArcs;
+	private ArrayList<Transition> transitions;
+	
+	public void addPlace(Integer jetons) {
+		Place place = new Place(jetons);
+		this.places.add(place);
+	}
+	
+	public void addInArcNormal(Place place, Integer poids) {
+		InArc arc = new InArc(place, poids);
+		this.inArcs.add(arc);
+	}
+	
+	public void addOutArc(Place place, Integer poids) {
+		OutArc arc = new OutArc(place, poids);
+		this.outArcs.add(arc);
+	}
+	
+	public void addZeroArc(Place place, Integer poids) {
+		Zero arc = new Zero(place, poids);
+		this.inArcs.add(arc);
+	}
+	
+	public void addVideurArc(Place place, Integer poids) {
+		Videur arc = new Videur(place, poids);
+		this.inArcs.add(arc);
+	}
+	
+	public void addTransition(ArrayList<InArc> inArcs, ArrayList<OutArc> outArcs) {
+		Transition transition = new Transition(inArcs, outArcs);
+		this.transitions.add(transition);
+	}
 }
