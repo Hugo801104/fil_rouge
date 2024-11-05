60ffa3edf3eb2d6bf795e2d5435e85c75564fb73
Hugo JASMIN <hugo.jasmin@imt-atlantique.net>
2024-11-05 23:22:52 +0100

    First Unit Tests, for arcs, places and transitions

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,26 +1,26 @@
-package Test;
+package test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import fil_rouge.*;
 
-public class Test2 {
-
+public class Test {
 	public static void main(String[] args) {
-		IPetriNet pn = new PetriNet();
-		Place p1 = pn.addPlace(new Place(8));
-		Place p2 = pn.addPlace(new Place(5));
+		Place p1 = new Place(8);
+		Place p2 = new Place(5);
 		System.out.println("Nombre de jetons initial de p1 : "+p1.getTokens());
 		System.out.println("Nombre de jetons initial de p2 : "+p2.getTokens());
-		InArc a1 = pn.addInArcNormal(p1, 3);
-		OutArc a2 = pn.addOutArc(p2, 9);
+		InArc a1 = new InArc(p1,3);
+		OutArc a2 = new OutArc(p2,9);
 		System.out.println("Poids de l'InArc : "+a1.getWeight());
 		System.out.println("Poids de l'OutArc : "+a2.getWeight());
-		Transition t1 = pn.addTransition(new Transition(new ArrayList<InArc>(Arrays.asList(a1)), new ArrayList<OutArc>(Arrays.asList(a2))));
+		ArrayList<InArc> ia = new ArrayList<InArc>();
+		ArrayList<OutArc> oa = new ArrayList<OutArc>();
+		ia.add(a1);
+		oa.add(a2);
+		Transition t1 = new Transition(ia,oa);
 		t1.fire();
 		System.out.println("Nombre de jetons final de p1 : "+p1.getTokens());
 		System.out.println("Nombre de jetons final de p2 : "+p2.getTokens());
 	}
-
 }
--- src/test/Test2.java
+++ src/test/Test2.java
@@ -1,26 +1,26 @@
-package Test;
+package test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import fil_rouge.*;
 
-public class Test {
+public class Test2 {
+
 	public static void main(String[] args) {
-		Place p1 = new Place(8);
-		Place p2 = new Place(5);
+		IPetriNet pn = new PetriNet();
+		Place p1 = pn.addPlace(new Place(8));
+		Place p2 = pn.addPlace(new Place(5));
 		System.out.println("Nombre de jetons initial de p1 : "+p1.getTokens());
 		System.out.println("Nombre de jetons initial de p2 : "+p2.getTokens());
-		InArc a1 = new InArc(p1,3);
-		OutArc a2 = new OutArc(p2,9);
+		InArc a1 = pn.addInArcNormal(p1, 3);
+		OutArc a2 = pn.addOutArc(p2, 9);
 		System.out.println("Poids de l'InArc : "+a1.getWeight());
 		System.out.println("Poids de l'OutArc : "+a2.getWeight());
-		ArrayList<InArc> ia = new ArrayList<InArc>();
-		ArrayList<OutArc> oa = new ArrayList<OutArc>();
-		ia.add(a1);
-		oa.add(a2);
-		Transition t1 = new Transition(ia,oa);
+		Transition t1 = pn.addTransition(new Transition(new ArrayList<InArc>(Arrays.asList(a1)), new ArrayList<OutArc>(Arrays.asList(a2))));
 		t1.fire();
 		System.out.println("Nombre de jetons final de p1 : "+p1.getTokens());
 		System.out.println("Nombre de jetons final de p2 : "+p2.getTokens());
 	}
+
 }
