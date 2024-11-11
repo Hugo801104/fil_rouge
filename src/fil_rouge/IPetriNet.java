16887d5d39845b7959db2b8415129184a013ee82
Pierrick289 <pierrick.causeret@imt-atlantique.net>
2024-11-11 10:45:56 +0100

    ajout du README

--- src/fil_rouge/IPetriNet.java
+++ src/fil_rouge/IPetriNet.java
@@ -38,31 +38,35 @@ public interface IPetriNet {
 	 * Add the Arc a to the PetriNet
 	 * 
 	 * @param p the Place linked to the Arc
+	 * @param t the Transition linked to the Arc
 	 * @param weight the weight of the arc
 	 */
-	OutArc addOutArc(Place p, int weight); // ou void addOutArc(...)
+	OutArc addOutArc(Place p, int weight); //OutArc addOutArc(Place p, Transition t, int weight);
 	
 	/**
 	 * Add the Arc a to the PetriNet
 	 * 
 	 * @param p the Place linked to the Arc
+	 * @param t the Transition linked to the Arc
 	 * @param weight the weight of the arc
 	 */
-	InArc addInArcNormal(Place p, int weight);
+	InArc addInArcNormal(Place p, int weight); //InArc addInArcNormal(Place p, Transition t, int weight);
 	
 	/**
 	 * Add the Arc a to the PetriNet
 	 * 
 	 * @param p the Place linked to the Arc
+	 * @param t the Transition linked to the Arc
 	 */
-	Empty addEmptyArc(Place p);
+	Empty addEmptyArc(Place p); //Empty addEmptyArc(Place p, Transition t);
 	
 	/**
 	 * Add the Arc a to the PetriNet
 	 * 
 	 * @param p the Place linked to the Arc
+	 * @param t the Transition linked to the Arc
 	 */
-	Zero addZeroArc(Place p);
+	Zero addZeroArc(Place p); //Zero addZeroArc(Place p, Transition t);
 	
 	/**
 	 * Remove the Arc a from the PetriNet
